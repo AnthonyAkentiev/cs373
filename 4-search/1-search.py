54d74010fd6763938b84625b7dc4f78c0f785e54
Anton Akentiev <anthony.akentiev@gmail.com>
2015-10-06 20:47:46 +0600

    Policy map

--- 4-search/1-search.py
+++ 4-search/1-search.py
@@ -32,12 +32,10 @@ delta = [[-1, 0], # go up
 delta_name = ['^', '<', 'v', '>']
 
 def search(grid,init,goal,cost):
-    # ----------------------------------------
-    # insert code here
-    # ----------------------------------------
      closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
      closed[init[0]][init[1]] = 1
      expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
+     action = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
 
      x = init[0]
      y = init[1]
@@ -79,11 +77,30 @@ def search(grid,init,goal,cost):
                                    g2 = g + cost
                                    open.append([g2, x2, y2])
                                    closed[x2][y2] = 1
+                                   action[x2][y2] = i
 
-     for i in range(len(expand)):
-          print expand[i]
+     #for i in range(len(expand)):
+     #     print expand[i]
+
+     policy = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
+
+     # go backwards
+     x = goal[0]
+     y = goal[1]
+     policy[x][y] = '*'
+
+     while x!=init[0] or y!=init[1]:
+          x2 = x - delta[action[x][y]][0]
+          y2 = y - delta[action[x][y]][1]
+          policy[x2][y2] = delta_name[action[x][y]]
+          x = x2
+          y = y2
+
+     for i in range(len(policy)):
+          print policy[i]
 
      return expand
 
 # Start
 print search(grid,init,goal,cost)
+
